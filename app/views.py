3984228aeb6787e7a2ae44a457a6c8ba89d52533
Jacob Allen <jallen25@basecampcodingacademy.org>
2024-11-13 22:42:39 -0600

    last night's changes 11/13

--- app/views.py
+++ app/views.py
@@ -63,30 +63,78 @@ from .models import RecipeData
 def filter_recipes(request):
     context = {}
 
-    
+    recipe_prepping_time_in_minutes = get_recipe_time_minutes(request)
     recipe_authors = get_recipe_authors(request)
+    recipe_temperature = get_cooking_temp(request)
 
     
     if request.method == "POST":
         all_data = []
 
-        
+        prep_time_in_minutes = request.POST.get('prep_time_in_minutes')
         recipe_author = request.POST.get('recipe_author')
+        recipe_temp = request.POST.get('cooking_temp')
+
+        #mintemp = request.POST.getlist("min_temp")
+        #maxtemp = request.POST.getlist("max_temp")
+
+        
+        mintemp = request.POST.get("min_temp")
+        maxtemp = request.POST.get("max_temp")
+        if mintemp != None and maxtemp!=None:
+
+            mintemp = int(mintemp)
+            maxtemp = int(maxtemp)
+
+        
+
+            temp = []
+
+            for i in RecipeData.objects.iterator():
+                
+                if i.cooking_temp != None and i.cooking_temp >= mintemp and i.cooking_temp <= maxtemp:
+                    temp.append(i)
+                    
+                    
+
+                
+
+
+                    print(f"This is the objects {i} {i.cooking_temp}")
+                context['tempcheck'] = temp
+            
+
+                
+                
+            print(f"MinTemp:  {mintemp}")
+            print(f"MaxTemp: {maxtemp}")
+
         if recipe_author:
             all_recipes = RecipeData.objects.filter(recipe_author=recipe_author)
+            all_data.extend(all_recipes)
+
+        if prep_time_in_minutes:
+            all_recipes = RecipeData.objects.filter(prep_time_in_minutes=prep_time_in_minutes)
             all_data.extend(all_recipes)
 
         
         filter_type = request.POST.getlist("filterItem")
         if filter_type:
+            print("Success")
             for option in filter_type:
+                
                 all_recipes = RecipeData.objects.filter(recipe_type=option)
+                print(all_recipes)
                 all_data.extend(all_recipes)
+
+        
 
         context['result'] = all_data
 
     
     context['recipe_authors'] = recipe_authors
+
+    context['recipe_prepping_time_in_minutes'] = recipe_prepping_time_in_minutes
 
     
     context['recipes'] = RecipeData.objects.all()
@@ -100,6 +148,47 @@ def get_recipe_authors(request):
 
     
     return recipe_authors
+
+def get_recipe_time_minutes(request):
+    recipe_prepping_time_in_minutes = RecipeData.objects.values_list('prep_time_in_minutes', flat=True).distinct()
+
+    return recipe_prepping_time_in_minutes
+    
+
+def get_cooking_temp(request):
+    recipe_cooking_temp = RecipeData.objects.values_list('cooking_temp', flat=True).distinct()
+
+    return recipe_cooking_temp
+
+def recipe_view(request):
+    
+    recipe_authors = RecipeData.objects.values_list('recipe_author', flat=True).distinct()
+    
+    selected_author = None
+    
+    if request.method == 'POST':
+        selected_author = request.POST.get('recipe_author')
+    
+    return render(request, 'filterrecipes.html', {
+        'recipe_authors': recipe_authors,
+        'selected_author': selected_author
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def built_in_choices(request):
     return render(request, 'builtinchoices.html')
